17da1cfde83000db4a89f71db293b6df49f50531
UranoDev <u@urano.dev>
2023-10-15 23:59:29 -0600

    Sin Taxes, se inserta un IVA Exento

--- qmf4.php
+++ qmf4.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Quiero mi Factura V4
  * Plugin URI:        https://urano.dev
  * Description:       Integración de Woocommerce con los servicios de facturación en línea de Quiero mi Factura (<a href='https://quieromifactura.mx'>quieromifactura.mx</a>).
- * Version:           1.0.5
+ * Version:           1.0.6
  * Author:            Urano González
  * Author URI:        https://urano.dev/qmf
  * License:           GPL-2.0+
@@ -39,7 +39,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'QMF4_VERSION', '1.0.5' );
+define( 'QMF4_VERSION', '1.0.6' );
 
 /**
  * The code that runs during plugin activation.
